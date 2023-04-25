240a052136dd3b1b8dc0f35d463318ae3ad87bc1
emilysaboss <emily.gillott2@gmail.com>
2023-04-25 22:48:28 +0100

    Voronoi analysis of CaOx
    Voronoi notebook for CaOx
    Parameters for CaOx
    Normalised params saved for use in ROC curves

--- Crack analysis/main_voronoi.m
+++ Crack analysis/main_voronoi.m
@@ -2,13 +2,13 @@ function [] = main_voronoi()
     addpath('skeletonization\')
     addpath('voronoi_functions\')
 
-    path = '..\\data2-albumin/All images/';
+    path = '..\\data1/unsaturated/';
     image_files = dir(path);
     file_names = image_files;
     % the first two lines in the struct are '.' and '..'
     file_names(1) = [];
     file_names(1) = [];
-    file = fopen('voronoi_params2.txt', 'w');
+    file = fopen('CaOx_params_unsat.txt', 'w');
     fprintf(file, 'file, node, defect, ratio, # of sides\n');
     %
     % These files have been edited to show all the images in separate
@@ -16,9 +16,9 @@ function [] = main_voronoi()
     % images. I used image 8 for the first image in B or 36 for the first
     % image in F
 
-    show = false;
+    show = true;
 
-    for index = 1:length(file_names) %replace this with a number to not iterate
+    for index = 26 %1:length(file_names) %replace this with a number to not iterate
 
         image_name = file_names(index).name;
         image_name = append(path, image_name);
